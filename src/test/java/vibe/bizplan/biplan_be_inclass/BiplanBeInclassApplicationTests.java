4956d2b4fa2242ab09d3c7027214961f426ab8c9
DongJun <djlee1201@naver.com>
2025-12-14 17:04:49 +0900

    Bizplan 프로젝트 백엔드 개발 준비 (작업 컨텍스트 &  Spring Boot 프로젝트 생성)

---
+++ src/test/java/vibe/bizplan/biplan_be_inclass/BiplanBeInclassApplicationTests.java
@@ -0,0 +1,13 @@
+package vibe.bizplan.biplan_be_inclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiplanBeInclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
